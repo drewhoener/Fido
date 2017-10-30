9948e720b852a3cc52fc45d465ead31eada6e3ac
Drew Hoener <drewhoener@gmail.com>
2017-10-30 11:13:24 -0400

    updated test case

--- src/test/java/DownloadTest.java
+++ src/test/java/DownloadTest.java
@@ -1,17 +1,19 @@
 import com.drewhoener.artifact.MavenArtifact;
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 
+import java.io.IOException;
 import java.net.HttpURLConnection;
+
+import static org.junit.Assert.assertTrue;
 
 public class DownloadTest {
 
-    @Test
+    @Test(expected = IOException.class)
     public void testArtifactDownload() throws Exception {
 
         MavenArtifact artifact = new MavenArtifact("me.drewhoener", "GameCore", "1.4-SNAPSHOT", "jar");
         HttpURLConnection connection = artifact.getCompleteConnection("https://drewhoener.com/repo/repository/public/", "admin", "admin123");
-
-
+        assertTrue(connection != null);
     }
 
 }
